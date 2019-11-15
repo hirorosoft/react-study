72a6cb53671b3f2640cebcda5251314c6bea1479
Hiroshi.Nakai <hrsi.ad@gmail.com>
2019-11-15 21:20:04 +0900

    時計表示

--- src/Clock.js
+++ src/Clock.js
@@ -2,8 +2,34 @@ import React from 'react';
 
 
 class Clock extends React.Component {
+    
+    constructor(props) {
+        super(props)
+
+        this.now = new Date()
+
+        this.state = {
+            time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+        }
+
+        this.refresh = this.refresh.bind(this)
+    }
+
+
+    refresh() {
+        this.now = new Date()
+
+        this.setState((state) => ({
+            time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+        }))
+
+    }
+
     render() {
-        return <p>ウヒョヒョ</p>
+
+            
+
+        return <p onClick={this.refresh}>{this.state.time}</p>
     }
 }
 
