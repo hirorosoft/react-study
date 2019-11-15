6a3dfd259dda948fbcc06485ecb55c0b7d4c88f1
Hiroshi.Nakai <hrsi.ad@gmail.com>
2019-11-15 20:35:35 +0900

    Clock 追加

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import Clock from './Clock'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <Clock></Clock>
+    <div className="container text-center">
+      <Clock />
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
-class Clock extends React.Component {
-  render() {
-    return <p>ウヒョヒョ</p>
-  }
-}
 
 export default App;
---
+++ src/Clock.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Clock extends React.Component {
+    render() {
+        return <p>ウヒョヒョ</p>
+    }
+}
+
+
+export default Clock
+
